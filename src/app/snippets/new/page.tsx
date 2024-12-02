97e6e8c42a6e3d2a7218c3ca88cbf6b1bbd58921
Ross Nguyen <xhieu94@gmail.com>
2024-12-02 23:24:42 +0700

    Init commit

---
+++ src/app/snippets/new/page.tsx
@@ -0,0 +1,43 @@
+import { db } from '@/db';
+
+export default function SnippetCreatePage() {
+  async function createSnippet(formData: FormData) {
+    // this needs to be a server action!
+    'use server';
+
+    // check the user's inputs and make sure they are valid
+    const title = formData.get('title');
+
+    const code = formData.get('code');
+
+    // create a new record in the database
+
+    // redirect the user back to the root route
+  }
+
+  return <form>
+    <h3 className='font-bold m-3'>Create a Snippet</h3>
+
+    <div className='flex flex-col gap-4'>
+      <div className='flex gap-4'>
+        <label className='w-12' htmlFor='title'>Title</label>
+
+        <input
+          name='title'
+          className='border rounded p-2 w-full'
+        />
+      </div>
+
+      <div className='flex gap-4'>
+        <label className='w-12' htmlFor='code'>Code</label>
+
+        <textarea
+          name='code'
+          className='border rounded p-2 w-full'
+        />
+      </div>
+
+      <button type='submit' className='rounded p-2 bg-blue-200'>Create</button>
+    </div>
+  </form>
+}
